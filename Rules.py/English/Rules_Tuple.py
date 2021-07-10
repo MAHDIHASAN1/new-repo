e38d4d07bde42dfab45802d573951e91bfdfe973
unknown <mahmudhasan5406@gamil.com>
2021-07-10 19:47:06 +0600

    tupie

--- Rules.py/English/Rules_Tuple.py
+++ Rules.py/English/Rules_Tuple.py
@@ -1,10 +1,10 @@
 ###Rules_Tuple
-
+ 
 #* টোপলে  ভিতর  এই পজিশনে কি আছে তা জানার জন্য              {print(Tuple name[Item index number])}
 #* Find out how many codes are used for tuple                  {print(dir(Tuple name))}   
 #* To find out how many times an item is inside a tuplera      {print(Tuple name.count(2))} 
 #* Find out what position this item is inside the tuple        {print(Tuple name.index(7))} 
-#*  
+#* To find out how many items are in the basket                {print(len(Tuple name))}             
 #*  
 #* 
 #*  
