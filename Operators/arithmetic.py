0a6adbc8f5bf3d26335b4c0f4ac5fa07315bfc07
unknown <mahmudhasan5406@gamil.com>
2021-07-12 20:55:46 +0600

    creat operators folder

---
+++ Operators/arithmetic.py
@@ -0,0 +1,10 @@
+# +, -, *, /, %, //
+print("Plus:",10 + 10)
+print("Minus:",20 - 5)
+print("Multiply:",5 * 5)
+print("Division:",20 / 4)
+print("Remainder:",5 % 2)
+print("Floor division:",5 // 2)
+
+
+
---
+++ Rules.py/English/Rules_Operators.py
@@ -0,0 +1,5 @@
+###Ruler_Operators
+
+
+
+
