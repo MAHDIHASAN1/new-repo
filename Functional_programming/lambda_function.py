fe0f99a109625f0c784ee1c9f8360922f3223b64
unknown <mahmudhasan5406@gamil.com>
2021-08-04 21:49:18 +0600

    create new file

---
+++ Functional_programming/lambda_function.py
@@ -0,0 +1,73 @@
+# Pure Function
+# No State, Change, Mutablity, No side-effect
+# High Order Function (HOF)
+# Recursion
+
+# Functional Programming, Object Oriented programming
+
+# map, filter, reduce, zip, enumerate
+# lambda function
+# functool
+
+
+def add(a, b):
+    a = a ** 2
+    return a + b
+
+def add(a, b):
+    return a + b
+
+print(add(30, 40))
+
+# Anonymous Function
+# Single Expression
+
+print((lambda a, b: a + b)(30, 60))
+print((lambda a, b, c, d: a + b + c + d)(50, 56, 78, 34))
+
+multi = lambda e, f: e * f
+print(multi(57, 47))
+
+
+
+# sum, max, min
+# map, filter, reduce, all, any
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- advanced_python/generator.py
+++ advanced_python/generator.py
@@ -9,7 +9,21 @@ def Normal_hello():
 
 # Generator Function
 def gen_hello():
-    yield "Hello Mahdi Hasan"
+    n = 1
+    print("First world num:",n)
+    yield "Hello world"
+
+    n += 1
+    print("second world num:",n) 
+    yield "Hello second world"
+
+    n += 1
+    print("third world num:",n) 
+    yield "Hello third world"
+
+    n += 1
+    print("fourth world num:",n) 
+    yield "Hello fourth world"
 
 normal = Normal_hello()
 print("Normal:",normal)
@@ -17,9 +31,43 @@ print("Normal:",normal)
 # Generator
 
 gen = gen_hello()
-print("genera:",gen)
+print(next(gen))
+print(next(gen))
+print(next(gen))
 
 
+def gen_power_two():
+    number = 1
+    while number < 20:
+        yield number ** 2 
+        number += 1
+
+num_power = gen_power_two()
+
+print(next(num_power))
+print(next(num_power))
+print(next(num_power))
+print(next(num_power))
+
+for i in num_power:
+    print(i)
+
+nums = [1, 2, 3, 4, 5]
+nums = [num for num in range(1, 6 )]
+print(nums)
+
+gun_nums = (num for num in range(1, 20))
+print(gun_nums)
+
+print(next(gun_nums))
+print(next(gun_nums))
+print(next(gun_nums))
+print(next(gun_nums))
+
+for num in gun_nums:
+    print("Number:",num)
+
+    
 
 
 
